e683435d9f17c680e69fbb12410e1b4672c1d8bb
MatiasG <matiasgodoy072@gmail.com>
2022-09-10 12:03:44 -0400

    m

---
+++ 4-pokemonGame/src/components/PokemonOptions.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="options-container">
+    <ul>
+      <li
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        @click="$emit('selectionPokemon', pokemon.id)"
+      >
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    hola() {
+      // this.$emit()
+    },
+  },
+};
+</script>
